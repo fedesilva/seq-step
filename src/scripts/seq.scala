9fd97a0a25d1a367de159d58dd4ea55bc404b67a
federico silva <fede@counselytics.com>
2017-06-20 19:36:55 -0300

    adding steps manually, wip

--- src/scripts/seq.scala
+++ src/scripts/seq.scala
@@ -1,8 +1,26 @@
 
 
-import org.seqnote.api._
+import org.seqstep.api._
+
+import scala.collection.immutable.SortedMap
+import eu.timepit.refined._
 
 val seq = Sequencer.initialized
 
+val (pidx, p) = seq.patterns.head
+val (tidx, tr) = p.tracks.head
+val (cidx, ch) = tr.channels.collect{ case (idx, d: DrumChannel) => idx -> d}.head
 
+// [(1),2,3,4] [(5),6,7,8] [(9),10,11,12] [(13),14,15,16]
+// 1,5,9,13
 
+val vel = refineMV[MIDIRange](32)
+
+val newSteps = SortedMap[Int, Seq[DrumStep]](
+  1   -> Seq(DrumStep(vel)),
+  5   -> Seq(DrumStep(vel)),
+  9   -> Seq(DrumStep(vel)),
+  13  -> Seq(DrumStep(vel))
+)
+
+val nch = ch.copy(steps = newSteps)
