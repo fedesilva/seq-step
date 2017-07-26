3485c60d5de22069493e07138ea369572f4f885f
federico silva <fede@counselytics.com>
2017-07-26 15:47:48 -0300

    VoiceRenderer is PatternRenderer

--- src/main/scala/org/seqstep/api/Pattern.scala
+++ src/main/scala/org/seqstep/api/Pattern.scala
@@ -2,6 +2,7 @@ package org.seqstep.api
 
 import monocle.macros.Lenses
 
+import scala.collection.immutable
 import scala.collection.immutable.SortedMap
 
 sealed trait Pattern {
@@ -31,3 +32,85 @@ object DrumPattern {
   }
   
 }
+
+
+
+/** Pattern rendering type class allows rendering control steps before playback.
+  *
+  * Adds note off and does other relevant transformations like unifying contiguous full open notes.
+  *
+  */
+trait PatternRenderer[T <: Pattern] {
+  
+  // For each step, generate a corresponding note off step.
+  // The purpose of doing it once and not directly as needed is to have the computation done
+  // on a single place and not later during playback.
+  
+  def render(c: T): PatternRenderer.RenderedVoice
+  
+}
+
+object PatternRenderer {
+  
+  final case class RenderedVoice(steps: SortedIntMap[Vector[RenderedStep]])
+  
+  def render[T <: Pattern : PatternRenderer](c: T): RenderedVoice =
+    implicitly[PatternRenderer[T]].render(c)
+  
+  
+  implicit val drumPatternRenderer = new PatternRenderer[DrumPattern] {
+    
+    override def render(c: DrumPattern): RenderedVoice = {
+      
+      val steps = for {
+        (_, v)  <- c.voices
+        (is, s) <- v.steps
+      } yield {
+        
+        val on  = RenderedNoteStep(v.note, v.octave, s.velocity)
+        val off = RenderedNoteOffStep(v.note, v.octave)
+        val onOffSteps = Vector(on, off)
+        (is, onOffSteps)
+      }
+      
+      RenderedVoice(steps)
+      
+    }
+  }
+  
+  implicit val synthPatternRenderer = new PatternRenderer[SynthPattern] {
+    
+    override def render(c: SynthPattern): RenderedVoice = {
+      
+      // Get the on and off note steps
+      val pairs = (for {
+        (_, v)      <- c.voices
+        (idx, step) <- v.steps
+      } yield {
+        val nix = idx + step.duration
+        val on  = RenderedNoteStep(step.note, step.octave, step.velocity)
+        val off = RenderedNoteOffStep(step.note, step.octave)
+        Vector(idx -> on, nix -> off)
+      }).flatten
+      
+      // Group the index -> note pairs by index and
+      val ss =
+        pairs
+          .toVector
+          .groupBy(_._1)
+          .map {
+            case (i, ps) =>
+              val ss = ps.map {
+                case (_, s) => s
+              }
+              i -> ss
+          }
+  
+      RenderedVoice(SortedMap(ss.toSeq: _*))
+      
+    }
+    
+  }
+  
+}
+
--- src/main/scala/org/seqstep/api/Voice.scala
+++ src/main/scala/org/seqstep/api/Voice.scala
@@ -27,64 +27,6 @@ final case class DrumVoice(
 ) extends Voice
 
 
-final case class RenderedVoice(steps: SortedIntMap[Vector[RenderedStep]])
-
-
-/** Voice rendering type class allows rendering control steps before playback.
-  *
-  * Adds note off and does other relevant transformations like unifying contiguous full open notes.
-  *
-  */
-trait VoiceRenderer[T <: Voice] {
-  
-  // For each step, generate a corresponding note off step.
-  // The purpose of doing it once and not directly as needed is to have the computation done
-  // on a single place and not later during playback.
-  
-  def render(c: T): RenderedVoice
-  
-}
-
-object VoiceRenderer {
-  
-  def render[T <: Voice : VoiceRenderer](c: T): RenderedVoice =
-    implicitly[VoiceRenderer[T]].render(c)
-  
-  implicit val drumVoiceRenderer = new VoiceRenderer[DrumPattern] {
-    override def render(c: DrumPattern): RenderedVoice = {
-      
-      for {
-        (iv, v) <- c.voices
-        (is, s) <- v.steps
-      }
-      
-        val on  = RenderedNoteStep(c.note, c.octave, step.velocity)
-        val off = RenderedNoteOffStep(c.note,c.octave)
-        val onOffSteps  = Vector(on , off)
-        (idx, onOffSteps)
-      
-      RenderedVoice(steps)
-    
-    }
-  }
-  
-  implicit val synthVoiceRenderer = new VoiceRenderer[SynthVoice] {
-    override def render(c: SynthVoice): RenderedVoice = {
-      val ss = c.steps.flatMap { case (idx, step) =>
-        // new events, with their index
-        val nix = idx + step.duration
-        val on  = RenderedNoteStep(step.note, step.octave, step.velocity)
-        val off = RenderedNoteOffStep(step.note, step.octave)
-        Vector(idx -> on, nix -> off)
-          .groupBy(_._1)
-          .map{ case (k, v) => k -> v.map{ case (_, x) => x } }
-          
-      }
-      RenderedVoice(ss)
-    }
-  }
-  
-}
 
 
 
